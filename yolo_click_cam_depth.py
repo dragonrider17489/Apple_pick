665d1a9fbc57829bc18978dfc02b8e6baecd403a
dragonrider17489 <132342669+dragonrider17489@users.noreply.github.com>
2024-11-05 13:02:54 +0530

    Update yolo_click_cam_depth.py

--- yolo_click_cam_depth.py
+++ yolo_click_cam_depth.py
@@ -3,11 +3,11 @@ import numpy as np
 import cv2
 
 def main():
-    # Configure depth and color streams
+   
     pipeline = rs.pipeline()
     config = rs.config()
 
-    # Get device product line for setting a supporting resolution
+ 
     pipeline_wrapper = rs.pipeline_wrapper(pipeline)
     pipeline_profile = config.resolve(pipeline_wrapper)
     device = pipeline_profile.get_device()
@@ -16,30 +16,30 @@ def main():
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-    # Start streaming
+   
     pipeline.start(config)
 
     try:
         while True:
-            # Wait for a coherent pair of frames: depth and color
+            
             frames = pipeline.wait_for_frames()
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
             if not depth_frame or not color_frame:
                 continue
 
-            # Convert images to numpy arrays
+         
             depth_image = np.asanyarray(depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
 
             # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-            # Display the color image
+           
             cv2.namedWindow('RealSense Color', cv2.WINDOW_AUTOSIZE)
             cv2.imshow('RealSense Color', color_image)
 
-            # Display the depth colormap image
+        
             cv2.namedWindow('RealSense Depth', cv2.WINDOW_AUTOSIZE)
             cv2.imshow('RealSense Depth', depth_colormap)
 
@@ -53,7 +53,7 @@ def main():
                 print('Images saved.')
 
     finally:
-        # Stop streaming
+  
         pipeline.stop()
 
 if __name__ == "__main__":
